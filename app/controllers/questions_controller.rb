bb45c1a58cfe48ebf2a4843ac6372508ad50ee2c
nairdesigns <nairbdg@gmail.com>
2022-02-14 13:39:07 +0900

    working

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,5 +2,15 @@ class QuestionsController < ApplicationController
   def ask; end
 
   def home; end
-  def answer; end
+
+  def answer
+    @question = params[:question]
+    @answer = if @question == 'I am going to work'
+                'Great!'
+              elsif @question.include? '?'
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
+  end
 end
