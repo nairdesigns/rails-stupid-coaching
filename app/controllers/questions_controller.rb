d92eec1f22b57b93fac8e107c338323f603ebfa2
nairdesigns <nairbdg@gmail.com>
2022-02-14 12:33:23 +0900

    done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,5 @@ class QuestionsController < ApplicationController
   def ask; end
 
   def home; end
+  def answer; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # root to: 'questions#home'
   get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
 end
